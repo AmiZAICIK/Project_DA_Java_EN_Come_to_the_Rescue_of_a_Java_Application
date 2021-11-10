bc182b1fc41f9b757e1d9d0fb366144d9a60155f
ami <ami@zaicik.com>
2021-11-10 18:32:34 +0100

    hop

--- Project02Eclipse/src/com/hemebiotech/analytics/ResultsFileInitializer.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ResultsFileInitializer.java
@@ -11,21 +11,22 @@ import java.io.File;
 
 public class ResultsFileInitializer {
 
-	public static void createurDeFichierDeResultats(String fichierFinal) throws Exception {
-		// TODO Auto-generated method stub
-		String nomFichierOut = fichierFinal; // on stocke le nom du fichier dans un string
-		File f = new File(nomFichierOut); // on définit un objet comme le fichier
+	public static void createurDeFichierDeResultats(String fichierOut) throws Exception {
+
+		File f = new File(fichierOut); // on définit un objet comme le fichier
 		try {
 			f.delete();
+			System.out.println(f + "1");
 		} catch (Exception e) {
-
+			System.out.println("erreur");
 		} // on tente d'effacer le fichier. Si il existait, il est effacé. Si il
 			// n'existait pas, la gestion d'erreur permet de passer à la suite
 
-		File file = new File(nomFichierOut);
-		System.out.println(file);
-		file.createNewFile(); // on crée un beau fichier tout neuf avec le nom voulu, dans le répertoire
-								// racine du projet
+		// File file = new File(fichierOut);
+
+		f.createNewFile(); // on crée un beau fichier tout neuf avec le nom voulu, dans le répertoire
+		System.out.println(f);
+		System.out.println(f.getAbsolutePath());// racine du projet
 
 	}
 
